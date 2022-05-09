d4065d7455244feea1692dba03909a54e43137e1
J. Aller <allerj@new-horizons.local>
2022-05-09 22:04:14 +0000

    initial commit

---
+++ allerj.php
@@ -0,0 +1,281 @@
+<?php
+
+include 'search_replace.php';
+
+if ( isset( $_POST['test'] ) && $_POST['test'] && $_POST['namespace'] != '' )
+{
+
+	$ext = 'php,css,js,txt,md';
+	$old_package = 'j_aller';
+	$old_namespace = 'allerj';
+	$old_title = "AllerJ";
+	
+	$new_package = $_POST['package'];
+	$new_namespace = $_POST['namespace'];
+	$new_title = $_POST['title'];
+	
+	$base = 'dashSpeedPress';
+	$destination_dir = $new_namespace; 
+	$source_dir = $base;   
+
+
+	if (recursive_files_copy($source_dir, $destination_dir)) {
+	
+		$dir = $new_namespace;
+
+		if ( !isset( $_POST['recursive'] ) ) $_POST['recursive'] = 0;
+		if ( !isset( $_POST['whole'] ) ) $_POST['whole'] = 0;
+		if ( !isset( $_POST['case'] ) ) $_POST['case'] = 0;
+		if ( !isset( $_POST['regexp'] ) ) $_POST['regexp'] = 0;
+
+	// FOR Title
+		$test = new search_replace( $dir,
+									$old_title,
+									$new_title,
+									$ext,
+									$_POST['regexp'],
+									$_POST['recursive'],
+									$_POST['whole'],
+									$_POST['case'] );
+		$result = $test->get_results();
+	
+	// FOR Namespace
+		$test = new search_replace( $dir,
+									$old_namespace,
+									$new_namespace,
+									$ext,
+									$_POST['regexp'],
+									$_POST['recursive'],
+									$_POST['whole'],
+									$_POST['case'] );
+		$result = $test->get_results();
+	
+	// FOR Package
+		$test = new search_replace( $dir,
+									$old_package,
+									$new_package,
+									$ext,
+									$_POST['regexp'],
+									$_POST['recursive'],
+									$_POST['whole'],
+									$_POST['case'] );
+		$result = $test->get_results();
+
+
+	// Make and download ZIP file
+		$zip_file = $new_namespace.'.zip';
+		$rootPath = realpath($dir);
+		$zip = new ZipArchive();
+		$zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+		
+		$files = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator($rootPath),
+			RecursiveIteratorIterator::LEAVES_ONLY
+		);
+		
+		foreach ($files as $name => $file)
+		{
+			if (!$file->isDir())
+			{
+				$filePath = $file->getRealPath();
+				$relativePath = substr($filePath, strlen($rootPath) + 1);
+				$zip->addFile($filePath, $relativePath);
+			}
+		}
+		
+		$zip->close();
+		
+		// header('Content-Description: File Transfer');
+		// header('Content-Type: application/octet-stream');
+		// header('Content-Disposition: attachment; filename='.basename($zip_file));
+		// header('Content-Transfer-Encoding: binary');
+		// header('Expires: 0');
+		// header('Cache-Control: must-revalidate');
+		// header('Pragma: public');
+		// header('Content-Length: ' . filesize($zip_file));
+		// readfile($zip_file);
+		
+	}
+
+
+}
+
+
+function recursive_files_copy($source_dir, $destination_dir) 
+{ 
+  $dir = opendir($source_dir);  
+  @mkdir($destination_dir);  
+  while($file = readdir($dir)) 
+  {
+	if(($file != '.') && ($file != '..')) 
+	{  
+	  if(is_dir($source_dir.'/'.$file))  
+	  {  
+		recursive_files_copy($source_dir.'/'.$file, $destination_dir.'/'.$file); 
+	  }  
+	  else 
+	  {  
+		copy($source_dir.'/'.$file, $destination_dir.'/'.$file);  
+	  }  
+	}  
+  }  
+  closedir($dir); 
+  return true;
+}  
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>dashSpeedPress() Theme Generation</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<style type="text/css">
+<!--
+body
+{
+	min-width: 800px;
+	margin: 0 3%;
+	background: #fff;
+	color: #000;
+	font-family: verdana,arial,sans-serif;
+	font-size: 75%;
+}
+#container
+{
+	background: #efefef;
+	border: 1px solid #ccc;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+	padding: 5px;
+	clear: both;
+}
+#header
+{
+	margin-top: 0;
+	margin-bottom: 20px;
+	padding: 10px;
+	-moz-border-radius: 6px;
+	border-bottom-right-radius: 6px;
+	border-bottom-left-radius: 6px;
+	box-shadow: 0px 3px 1px #ccc;
+	background: #383838;
+	/* gecko based browsers */
+	background: -moz-linear-gradient(top, #444, #000);
+	/* webkit based browsers */
+	background: -webkit-gradient(linear, left top, left bottom, from(#444), to(#000));
+	color: #fff; /* text colour (black) */
+	height: auto; /* gradient uses the full height of the element */
+}
+#form_table
+{
+	width: 100%;
+	border-collapse: seperate;
+	border-spacing: 8px;
+}
+#form_table td
+{
+	-moz-border-radius: 3px;
+	border-radius: 3px;
+	background: #ddd;
+	color: #000;
+	padding: 5px;
+}
+input
+{
+	padding: 3px;
+}
+.align_elem
+{
+	text-align: left;
+}
+.align_ctrl
+{
+	text-align: center;
+}
+-->
+</style>
+</head>
+<body>
+	<div id="header">
+		<h1 style="font-style:oblique;padding-left:20px;">dashSpeedPress() Theme Generation</h1>
+	</div>
+	<div id="container">
+		<?php
+			if($_POST){
+				?>
+				<center>
+				<h1>Theme for <?= $new_title; ?> Ready</h1>
+				<h5><a href="<?= $new_namespace; ?>.zip">Download <?= $new_namespace; ?>.zip Now</a></h5>
+				</center>
+				<br /><br /><br /><br />
+				<?php
+			}
+		?>
+		<form action="dash.php" method="post">
+			<input type="hidden" name="test" value="1" />
+			<table id="form_table">
+				<tr>
+					<th class="align_elem" colspan="2">Use this to build a custom branded theme for a client.<br /><br />
+					</th>
+				</tr>
+				<tr>
+					<th class="align_elem" colspan="2">Client Title</th>
+				</tr>
+				<tr>
+					<td class="align_elem"><input type="text" name="title" /></td>
+					<td class="align_elem" style="width:100%;">This should be something like "Datz Restaurant" it replaces "dashSpeedPress()" in base theme.</td>
+				</tr>
+				<tr>
+					<th class="align_elem" colspan="2">Package</th>
+				</tr>
+				<tr>
+					<td class="align_elem"><input type="text" name="package" /></td>
+					<td class="align_elem">This is an underscore version of title "Datz_Restaurant" it replaces "dash_Speed_Press" in base theme.</td>
+				</tr>
+				<tr>
+					<th class="align_elem" colspan="2">Namespace</th>
+				</tr>
+				<tr>
+					<td class="align_elem"><input type="text" name="namespace" /></td>
+					<td class="align_elem">This should be short, unique, all lowercase, oneword, namespace like "datzr" it replaces "dashsp"</td>
+				</tr>
+				<tr>
+					<td colspan="2" class="align_ctrl"><input type="submit" value="Replace" style="padding:5px 20px;" />&nbsp;&nbsp;&nbsp;<input type="reset"style="padding:5px 16px;" value="Clear Form" /></td>
+				</tr>
+				<tr>
+					<th class="align_elem" colspan="2" style="padding:10px;">
+					<?php
+
+					if ( isset( $_POST['test'] ) && $_POST['test'] && $_POST['namespace'] != '' )
+					{
+						echo "Search string: <span style=\"font-weight:normal\">{$_POST['needle']}</span><br />";
+						echo "Replacement string: <span style=\"font-weight:normal\">{$_POST['replace']}</span><br />";
+						echo "<p>Files searched: {$result[0]}<br />";
+						echo "Files modified: {$result[1]}</p>";
+
+						echo "<p style=\"height:100px;overflow:auto;background:#fff;padding:8px;border:1px solid #ccc;\">";
+
+						if ( $result[1] )
+						{
+							foreach ( $result[2] as $files )
+								echo $files . ' instance(s)<br />';
+						}
+
+						echo '</p>';
+					}
+					else
+					{
+						echo "<p>Files searched:<br />";
+						echo "Files modified:</p>";
+					}
+
+					?>
+					</th>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+</html>
---
+++ search_replace.php
@@ -0,0 +1,526 @@
+<?php
+
+/**
+ * Search and replace class for text based files
+ *
+ * @package PHP Search & Replace
+ * @version 1.0.1
+ * @author MT Jordan <mtjo62@gmail.com>
+ * @copyright 2014
+ * @license zlib/libpng
+ * @link
+ */
+
+class search_replace
+{
+	/**********************************************
+	 * Internal private variables
+	 *********************************************/
+
+	/**
+	 * Path of directory to be searched
+	 *
+	 * @access private
+	 * @var    str
+	 */
+	private $dir_path;
+
+	/**
+	 * User defined array of inclusive extensions
+	 *
+	 * @access private
+	 * @var    array
+	 */
+	private $set_extension = array();
+
+	/**
+	 * Array of search file paths
+	 *
+	 * @access private
+	 * @var    array
+	 */
+	private $file_array;
+
+	/**
+	 * Count of valid files to be searched
+	 *
+	 * @access private
+	 * @var    int
+	 */
+	private $file_cnt = 0;
+
+	/**
+	 * Recursive iterator type flag
+	 *
+	 * @access private
+	 * @var    bool
+	 */
+	private $set_recursive = true;
+
+	/**
+	 * Match case flag
+	 *
+	 * @access private
+	 * @var    bool
+	 */
+	private $set_case = false;
+
+	/**
+	 * Match whole word flag
+	 *
+	 * @access private
+	 * @var    bool
+	 */
+	private $set_whole_word = false;
+
+	/**
+	 * Count of modified files
+	 *
+	 * @access private
+	 * @var    mixed
+	 */
+	private $mod_cnt = 0;
+
+	/**
+	 * Use regular expression flag
+	 *
+	 * @access private
+	 * @var    bool
+	 */
+	private $set_regex;
+
+	/**
+	 * String to be searched - needle
+	 *
+	 * @access private
+	 * @var    mixed
+	 */
+	private $search_needle;
+
+	/**
+	 * Replacement string
+	 *
+	 * @access private
+	 * @var    mixed
+	 */
+	private $search_replacement;
+
+	/**********************************************
+	 * Constructor
+	 *********************************************/
+
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 * @param  str   $dir
+	 * @param  mixed $needle
+	 * @param  mixed $replacement
+	 */
+	public function __construct( $dir, $needle, $replacement )
+	{
+		$this->dir_path = $dir;
+		$this->search_needle = trim( $needle );
+		$this->search_replacement = trim( $replacement );
+	}
+
+	/**********************************************
+	 * Private methods
+	 *********************************************/
+
+	/**
+	 * Verify occurance of needle in string using regex or strpos
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function count_needle( $string )
+	{
+		$needle_cnt = false;
+
+		if ( $this->set_regex )
+		{
+			$needle_cnt = $this->count_needle_regex( $string );
+		}
+		elseif ( $this->set_whole_word )
+		{
+			$needle_cnt = $this->count_needle_whole_word( $string );
+		}
+		else
+		{
+			$needle_cnt = $this->count_needle_default( $string );
+		}
+
+		return $needle_cnt;
+	}
+
+	/**
+	 * Verify occurance of needle in string
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function count_needle_default( $string )
+	{
+		if ( $this->set_case )
+		{
+			$needle_cnt = strpos( $string, $this->search_needle );
+		}
+		else
+		{
+			$needle_cnt = stripos( $string, $this->search_needle );
+		}
+
+		return ( $needle_cnt !== false ) ? true : false;
+	}
+
+	/**
+	 * Verify occurance of needle in string using regex
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function count_needle_regex( $string )
+	{
+		$expression = ( $this->set_case ) ? "`{$this->search_needle}`uU" : "`{$this->search_needle}`iuU";
+
+		return ( preg_match( $expression, $string ) ) ? true : false;
+	}
+
+	/**
+	 * Verify occurance of whole word in string
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function count_needle_whole_word( $string )
+	{
+		$needle = preg_quote( $this->search_needle );
+		$expression = ( $this->set_case ) ? "`\b$needle\b`uU" : "`\b$needle\b`iuU";
+
+		return ( preg_match( $expression, $string ) ) ? true : false;
+	}
+
+	/**
+	 * Determine that file is text/* mime type
+	 *
+	 * @access private
+	 * @param  mixed $file
+	 * @param  str   $ext
+	 * @return bool
+	 */
+	private function get_mime_type( $file )
+	{
+		$return_value = false;
+
+		//determine if file is text/* mime type - fall back to the deprecated mime_content_type if fileinfo extension not enabled
+		if ( function_exists( 'finfo_open' ) )
+		{
+			$return_value = ( substr_count( finfo_file( finfo_open( FILEINFO_MIME_TYPE ), $file ), 'text/' ) ) ? true : false;
+		}
+		else
+		{
+			$return_value = ( substr_count( mime_content_type( $file ), 'text/' ) ) ? true : false;
+		}
+
+		return ( $this->set_mime ) ? true : $return_value;
+	}
+
+	/**
+	 * Iterates through directory and saves file if it meets search/replace arguments
+	 *
+	 * @access private
+	 * @return null
+	 */
+	private function iterator()
+	{
+		$dir = new DirectoryIterator( $this->dir_path );
+
+		while ( $dir->valid() )
+		{
+			if ( $this->validate_file( $dir ) )
+			{
+				$this->write_to_file( $dir->getPathname(), $this->string_replace( file_get_contents( $dir->getPathname() ), $dir->key() ) );
+			}
+
+			$dir->next();
+		}
+	}
+
+	/**
+	 * Recursively iterates through directory and saves file if it meets search/replace arguments
+	 *
+	 * @access private
+	 * @return null
+	 */
+	private function recursive_iterator()
+	{
+		$dir = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $this->dir_path ) );
+
+		while ( $dir->valid() )
+		{
+			if ( $this->validate_file( $dir ) )
+			{
+				$this->write_to_file( $dir->key(), $this->string_replace( file_get_contents( $dir->key() ), $dir->key() ) );
+			}
+
+			$dir->next();
+		}
+	}
+
+	/**
+	 * Performs string replacement functions
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @param  str $path
+	 * @return mixed
+	 */
+	private function string_replace( $string, $path )
+	{
+		$return_value = false;
+
+		if ( $this->count_needle( $string ) )
+		{
+			if ( $this->set_regex )
+			{
+				$replace = $this->string_replace_regex( $string );
+			}
+			elseif ( $this->set_whole_word )
+			{
+				$replace = $this->string_replace_whole_word( $string );
+			}
+			else
+			{
+				$replace = $this->string_replace_default( $string );
+			}
+
+			if ( $replace !== false )
+			{
+				$this->mod_cnt++;
+				$this->file_array[] = $path . ' - ' . $replace[1];
+				$return_value = $replace[0];
+			}
+		}
+
+		return $return_value;
+	}
+
+	/**
+	 * Default string replacement
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function string_replace_default( $string )
+	{
+		$count = 0;
+
+		if ( $this->set_case )
+		{
+			$replace = str_replace( $this->search_needle, $this->search_replacement, $string, $count );
+		}
+		else
+		{
+			$replace = str_ireplace( $this->search_needle, $this->search_replacement, $string, $count );
+		}
+
+		return ( is_string( $replace ) ) ? array( $replace, $count ) : false;
+	}
+
+	/**
+	 * Regex string replacement
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function string_replace_regex( $string )
+	{
+		$count = 0;
+		$expression = ( $this->set_case ) ? "`{$this->search_needle}`u" : "`{$this->search_needle}`iu";
+
+		$replace = preg_replace( $expression, $this->search_replacement, $string, -1, $count );
+
+		return ( $replace !== null ) ? array( $replace, $count ) : false;
+	}
+
+	/**
+	 * Whole word string replacement
+	 *
+	 * @access private
+	 * @param  str $string
+	 * @return mixed
+	 */
+	private function string_replace_whole_word( $string )
+	{
+		$count = 0;
+		$needle = preg_quote( $this->search_needle );
+		$expression = ( $this->set_case ) ? "`\b$needle\b`u" : "`\b$needle\b`iu";
+
+		$replace = preg_replace( $expression, $this->search_replacement, $string, -1, $count );
+
+		return ( $replace !== null ) ? array( $replace, $count ) : false;
+	}
+
+	/**
+	 * Determine if file has valid extension, is text/* mime type and is readable/writable
+	 *
+	 * @access private
+	 * @param  mixed $dir
+	 * @return bool
+	 */
+	private function validate_file( $dir )
+	{
+		$return_value = false;
+		$valid_ext = true;
+
+		//if PHP version < than 5.3.6, use pathinfo() to get extension
+		if ( version_compare( phpversion(), '5.3.6', '<' ) )
+		{
+			$path_parts = pathinfo( $dir->getPathname() );
+			$file_ext = $path_parts['extension'];
+		}
+		else
+		{
+			$file_ext = $dir->getExtension();
+		}
+
+		//determine if user defined extension was requested
+		if ( count( $this->set_extension ) )
+		{
+			$valid_ext = ( in_array( $file_ext, $this->set_extension ) ) ? true : false;
+		}
+
+		//determine if is file, is readable and writable, is text/* mime type and an inclusive extension
+		if ( !$dir->isDot() && $dir->isFile() && $dir->isReadable() && $dir->isWritable()&& $this->get_mime_type( $dir->getPathname() ) && $valid_ext )
+		{
+			$this->file_cnt++;
+			$return_value = true;
+		}
+
+		return $return_value;
+	}
+
+	/**
+	 * Write modified string to file
+	 *
+	 * @access public
+	 * @param  str $path
+	 * @param  str $replace
+	 * @return array
+	 */
+	private function write_to_file( $path, $replace )
+	{
+		if ( $replace !== false )
+		{
+			file_put_contents( $path, $replace );
+		}
+	}
+
+	/**********************************************
+	 * Public methods
+	 *********************************************/
+
+	/**
+	 * Perform search & replace and return results
+	 *
+	 * @access public
+	 * @return array
+	 */
+	public function get_results()
+	{
+		if ( $this->set_recursive )
+		{
+			$this->recursive_iterator();
+		}
+		else
+		{
+			$this->iterator();
+		}
+
+		//return number of files searched, modified, file path and number of replace instances
+		return array( $this->file_cnt, $this->mod_cnt, $this->file_array );
+	}
+
+	/**
+	 * Set case-sensitive flag
+	 *
+	 * @access public
+	 * @param  bool $case
+	 * @return bool
+	 */
+	public function set_case( $case=false )
+	{
+		$this->set_case = $case;
+	}
+
+	/**
+	 * Set user defined inclusive extensions to search
+	 *
+	 * @access public
+	 * @param  str $extension
+	 * @return array
+	 */
+	public function set_extension( $extension=null )
+	{
+		//setting this variable will overide $this->set_mime
+		$this->set_extension = array_filter( explode( ',', strtolower( $extension ) ) );
+	}
+
+	/**
+	 * Set MIME type flag
+	 *
+	 * @access public
+	 * @param  bool $mime
+	 * @return bool
+	 */
+	public function set_mime( $mime=false )
+	{
+		$this->set_mime = $mime;
+	}
+
+	/**
+	 * Set search with regex flag
+	 *
+	 * @access public
+	 * @param  bool $regex
+	 * @return bool
+	 */
+	public function set_regex( $regex=false )
+	{
+		$this->set_regex = $regex;
+	}
+
+	/**
+	 * Set recursive iterator flag
+	 *
+	 * @access public
+	 * @param  bool $recursive
+	 * @return bool
+	 */
+	public function set_recursive( $recursive=true )
+	{
+		$this->set_recursive = $recursive;
+	}
+
+	/**
+	 * Set search whole word flag
+	 *
+	 * @access public
+	 * @param  bool $whole_word
+	 * @return bool
+	 */
+	public function set_whole_word( $whole_word=false )
+	{
+		$this->set_whole_word = $whole_word;
+	}
+}
+
+/* EOF search_replace.php */
+/* Location: ./search_replace.php */
