5bd3da62b1734b362be365ca77c0e5505f83a23a
J. Aller <allerj@new-horizons.local>
2022-05-09 22:05:49 +0000

    update to spacing

--- allerj.php
+++ allerj.php
@@ -125,7 +125,6 @@ function recursive_files_copy($source_dir, $destination_dir)
 
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
